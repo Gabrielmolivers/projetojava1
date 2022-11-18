994527f6b07ee2348f02d51ac16e22473e20171e
Auto Info <Auto Info@DESKTOP-CBQUMTR>
2022-11-18 17:11:49 -0200

    Primeira Definição de variaveis

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -4,5 +4,21 @@ public class PrimeiraClasseJava {
 
 	public static void main(String[] args) {
 	   System.out.println("Meu primeiro programa em Java");
+	
+	int maiorIdade = 18;
+	
+	int idosoIdade = 60;
+	
+	int jovemIdade = 16;
+	
+	int criancaIdade = 7;
+	
+	String cpf = "102.823.969-69";
+	
+	System.out.println("idade de um adulto é = " + maiorIdade);
+	System.out.println("de um velho é = " + idosoIdade);
+	System.out.println("meu CPF é " + cpf);
+	
+	
 	}
 }
