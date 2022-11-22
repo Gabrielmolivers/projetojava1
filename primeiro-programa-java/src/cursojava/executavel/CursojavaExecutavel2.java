39a8ba4b1be04584c858717fe85c3fefe7475d56
Auto Info <Auto Info@DESKTOP-CBQUMTR>
2022-11-22 11:52:43 -0200

    Commit para salvar

--- primeiro-programa-java/src/cursojava/executavel/CursojavaExecutavel2.java
+++ primeiro-programa-java/src/cursojava/executavel/CursojavaExecutavel2.java
@@ -4,9 +4,12 @@ public class CursojavaExecutavel2 {
 	
 	public static void main(String[] args) {
 		
-		Aluno aluno1;
+		Aluno aluno1 = new Aluno();
 		
 		Aluno aluno2 = new Aluno();
+		
+		Aluno aluno3 = new Aluno();
+		
 	}
 
 }
