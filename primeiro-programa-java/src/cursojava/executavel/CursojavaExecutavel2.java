c5cfe03ff40ae0aaf453f8e4323d9af804023c67
Auto Info <Auto Info@DESKTOP-CBQUMTR>
2022-11-22 17:45:03 -0200

    Jop informações do aluno

--- primeiro-programa-java/src/cursojava/executavel/CursojavaExecutavel2.java
+++ primeiro-programa-java/src/cursojava/executavel/CursojavaExecutavel2.java
@@ -1,39 +1,49 @@
 package cursojava.executavel;
 
+import javax.swing.JOptionPane;
+
 import cursojava.classes.Aluno;
 
 public class CursojavaExecutavel2 {
 	
 	public static void main(String[] args) {
 		
+		String nome = JOptionPane.showInputDialog("Nome do Aluno:");
+		String idade = JOptionPane.showInputDialog("Idade:");
+		String registroGeral = JOptionPane.showInputDialog("RG:");
+		String numeroCpf = JOptionPane.showInputDialog("CPF:");
+		String nomeMae = JOptionPane.showInputDialog("Nome da mãe:");
+		String nomePai = JOptionPane.showInputDialog("Nome do pai:");
+		String dataMatricula = JOptionPane.showInputDialog("Data da matrícula:");
+		String serieMatriculado = JOptionPane.showInputDialog("Classe matriculado:");
+		String nomeEscola = JOptionPane.showInputDialog("Escola:");
+		String nota1 = JOptionPane.showInputDialog("Nota 1º Bimestre:");
+		String nota2 = JOptionPane.showInputDialog("Nota 2º Bimestre:");
+		String nota3 = JOptionPane.showInputDialog("Nota 3º Bimestre:");
+		String nota4 = JOptionPane.showInputDialog("Nota 4º Bimestre:");
+		
+		
 		Aluno aluno1 = new Aluno();
-		aluno1.setNome("Marcelo");
-		aluno1.setIdade(31);
+		aluno1.setNome(nome);
+		aluno1.setIdade(Integer.valueOf(idade));
 		aluno1.setDataNascimento("18/09/1998");
-		aluno1.setRegistroGeral("123.123.13.1325");
-		aluno1.setNumeroCpf("1232113624654");
-		aluno1.setNomeMae("Claudia");
-		aluno1.setNomePai("William");
-		aluno1.setDataMatricula("10/10/2022");
-		aluno1.setSerieMatriculado("8");
-		aluno1.setNomeEscola("Escola Rio Branco");
-		aluno1.setNota1(80);
-		aluno1.setNota2(70);
-		aluno1.setNota3(58);
-		aluno1.setNota4(90);
+		aluno1.setRegistroGeral(registroGeral);
+		aluno1.setNumeroCpf(numeroCpf);
+		aluno1.setNomeMae(nomeMae);
+		aluno1.setNomePai(nomePai);
+		aluno1.setDataMatricula(dataMatricula);
+		aluno1.setSerieMatriculado(serieMatriculado);
+		aluno1.setNomeEscola(nomeEscola);
+		aluno1.setNota1(Integer.valueOf(nota1));
+		aluno1.setNota2(Integer.valueOf(nota2));
+		aluno1.setNota3(Integer.valueOf(nota3));
+		aluno1.setNota4(Integer.valueOf(nota4));
 		
 				
-		System.out.println("Nome é = " + aluno1.getNome());
-		System.out.println("Media da nota é = " + aluno1.getMediaNota());
-		System.out.println("O aluno está aprovado? " + (aluno1.getAlunoAprovado() ? "Aprovado" : "Reprovado"));
+		System.out.println("Aluno: " + aluno1.getNome());
+		System.out.println("Média: " + aluno1.getMediaNota());
+		System.out.println("O aluno esta " + (aluno1.getAlunoAprovado() ? "Aprovado" : "Reprovado"));
 	
-		Aluno aluno2 = new Aluno();
-		
-		Aluno aluno3 = new Aluno();
-		
-		Aluno aluno4 = new Aluno("Gabriel");
-		
-		Aluno aluno5 = new Aluno("Nathaly", 25);
 		
 	}
 
