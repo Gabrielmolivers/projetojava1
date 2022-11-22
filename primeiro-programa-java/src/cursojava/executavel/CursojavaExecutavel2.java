4afdfd3349a7c8089074dce5f9ac78b06bc86544
Auto Info <Auto Info@DESKTOP-CBQUMTR>
2022-11-22 16:11:49 -0200

    Commit para deixar Salvo

--- primeiro-programa-java/src/cursojava/executavel/CursojavaExecutavel2.java
+++ primeiro-programa-java/src/cursojava/executavel/CursojavaExecutavel2.java
@@ -1,14 +1,33 @@
 package cursojava.executavel;
 
+import cursojava.classes.Aluno;
+
 public class CursojavaExecutavel2 {
 	
 	public static void main(String[] args) {
 		
 		Aluno aluno1 = new Aluno();
+		aluno1.setNome("Marcelo");
+		aluno1.setIdade(31);
+		aluno1.setDataNascimento("18/09/1998");
+		aluno1.setRegistroGeral("123.123.13.1325");
+		aluno1.setNumeroCpf("1232113624654");
+		aluno1.setNomeMae("Claudia");
+		aluno1.setNomePai("William");
+		aluno1.setDataMatricula("10/10/2022");
+		aluno1.setSerieMatriculado("8");
+		aluno1.setNomeEscola("Escola Rio Branco");
 		
+				
+		System.out.println("Nome é = " + aluno1.getNome());
+	
 		Aluno aluno2 = new Aluno();
 		
 		Aluno aluno3 = new Aluno();
+		
+		Aluno aluno4 = new Aluno("Gabriel");
+		
+		Aluno aluno5 = new Aluno("Nathaly", 25);
 		
 	}
 
