065479c74d27330ed8a7758ce9cc62ba59381b0c
Auto Info <Auto Info@DESKTOP-CBQUMTR>
2022-12-06 18:15:46 -0200

    Criando primeira tela

---
+++ primeiro-programa-java/src/cursojava/thread/TelaTimeThread.java
@@ -0,0 +1,24 @@
+package cursojava.thread;
+
+import java.awt.Dimension;
+
+import javax.swing.JDialog;
+
+public class TelaTimeThread extends JDialog {
+	
+	
+	public TelaTimeThread() {
+		setTitle("Tela de Teste Time/Thread");
+		setSize(new Dimension(300, 315));
+		setLocationRelativeTo(null);
+		setResizable(false);
+		/*Parametrização da tela, comece os componentes abaixo*/
+		
+		
+		
+		
+		
+		setVisible(true);
+	}
+
+}
