bd07e86db9b5172a4b8d1491236e93b27bda3c33
Auto Info <Auto Info@DESKTOP-CBQUMTR>
2022-12-06 17:58:59 -0200

    COMMIT ANTES DE APAGAR E FAZER MANUAL TUDO DO ZERO

--- primeiro-programa-java/src/cursojava/thread/AulaThread.java
+++ primeiro-programa-java/src/cursojava/thread/AulaThread.java
@@ -6,50 +6,57 @@ public class AulaThread {
 
 	public static void main(String[] args) throws InterruptedException {
 
-		new Thread() {
+		Thread threadEmail = new Thread(thread1);
+		threadEmail.start();
 
-			public void run() {
-
-				for (int pos = 0; pos < 10; pos++) {
-
-					try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-
-					System.out.println("Executando alguma rotina, por exemplo envio de e-mail");
-
-				}
-				System.out.println("CHEGOU AO FIM DO CÓDIGO DE TESTE DE THREAD.");
-			};
-
-		}.start();
-
-		new Thread() {
-
-			public void run() {
-
-				for (int pos = 0; pos < 10; pos++) {
-
-					try {
-						Thread.sleep(1000);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-
-					System.out.println("Executando a rotina numero 2, faturamento");
-
-				}
-				System.out.println("CHEGOU AO FIM DO CÓDIGO DE TESTE DE THREAD 2.");
-			};
-
-		}.start();
+		Thread threadFaturamento = new Thread(thread2);
+		threadFaturamento.start();
 
 		JOptionPane.showMessageDialog(null, "Sistema continua executando para o usuário");
 
 	}
+
+	private static Runnable thread2 = new Runnable() {
+
+		@Override
+		public void run() {
+
+			for (int pos = 0; pos < 10; pos++) {
+
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+				System.out.println("Executando a rotina numero 2, faturamento");
+
+			}
+			System.out.println("CHEGOU AO FIM DO CÓDIGO DE TESTE DE THREAD 2.");
+
+		}
+	};
+
+	private static Runnable thread1 = new Runnable() {
+
+		@Override
+		public void run() {
+			for (int pos = 0; pos < 10; pos++) {
+
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+
+				System.out.println("Executando alguma rotina, por exemplo envio de e-mail");
+
+			}
+			System.out.println("CHEGOU AO FIM DO CÓDIGO DE TESTE DE THREAD.");
+
+		}
+	};
 
 }
